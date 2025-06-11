874f5286ac73fd65a541277387450a0aad9d100c
shimaa2468 <shamazourob@gmail.com>
2025-06-11 08:39:01 -0700

    fix bug2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Food-mangment/",
+  base:"/Food-mangment/Log-in",
   plugins: [react()],
 })
